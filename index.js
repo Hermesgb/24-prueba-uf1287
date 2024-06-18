3861c588ebf7fd613165a576c0f23ad26c584cdf
Hermesgb <hermesgabriel@gmail.com>
2024-06-18 13:11:13 +0200

    Multiplicación de parámetros

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
     var sumando1 = Number(req.query.sumando1)
     var sumando2 = Number(req.query.sumando2)
-    var suma = sumando1 + sumando2
-    var sumaString = suma.toString()
-    res.send(sumaString)
+    var multiplicacion = sumando1 * sumando2
+    var multiplicaString = multiplicacion.toString()
+    res.send(multiplicaString)
 })
 
 app.listen(port, () => {
