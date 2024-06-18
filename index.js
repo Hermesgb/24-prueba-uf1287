df822f70db2903197145ec2261ea11b52f2151f4
Hermesgb <hermesgabriel@gmail.com>
2024-06-18 13:09:43 +0200

    Suma de parametros

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    var sumando1 = Number(req.query.sumando1)
+    var sumando2 = Number(req.query.sumando2)
+    var suma = sumando1 + sumando2
+    var sumaString = suma.toString()
+    res.send(sumaString)
 })
 
 app.listen(port, () => {
     console.log('App listening at htpp://localhost:${port}')
 })
 
-app.use(express.static('public'));
